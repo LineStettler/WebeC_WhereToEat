303e2a81e55eda1195d702768b61ec729e7a4cc2
Elena Mastrandrea <elena.mastrandrea@students.fhnw.ch>
2016-04-12 16:22:32 +0200

    Geolocation ermitteln und Google Map in den Where-Screen laden

--- app.js
+++ app.js
@@ -11,12 +11,46 @@ function showSection(id){
     $(id).show();
 }
 
+function success(position) {
+    var mapcanvas = document.createElement('div');
+    mapcanvas.id = 'mapcontainer';
+    mapcanvas.style.height = '550px';
+    mapcanvas.style.width = '500px';
+
+    document.querySelector(WHERE).appendChild(mapcanvas);
+
+    var here = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    var options = {
+        zoom: 15,
+        center: here,
+        mapTypeControl: false,
+        navigationControlOptions: {
+            style: google.maps.NavigationControlStyle.SMALL
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("mapcontainer"), options);
+
+    var marker = new google.maps.Marker({
+        position: here,
+        map: map,
+        title:"You are here!"
+    });
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success);
+} else {
+    error('Geo Location is not supported');
+}
+
 $(document).ready(function() {
 
-    showSection(what);
+    showSection(WHAT);
 
-    $('#What-link').click( showSection(what));
-    $('#Who-link').click( showSection(who));
-    $('#Where-link').click( showSection(where));
+    $('#What-link').click( showSection(WHAT));
+    $('#Who-link').click( showSection(WHO));
+    $('#Where-link').click( showSection(WHERE));
 
 });

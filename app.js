083153fa8f348a7507bcbedfb606893cf4ff9743
Line Stettler <line.stettler@students.fhnw.ch>
2016-04-12 15:27:04 +0200

    js method show section works()

--- app.js
+++ app.js
@@ -1,50 +1,22 @@
 /**
  * Created by Line on 05/04/16.
  */
-
+var WHAT = "#What";
+var WHERE = "#Where";
+var WHO = "#Who";
 
 function showSection(id){
 
-    $("#What").hide();
-    $("#Where").hide();
-    $("#Who").hide();
-    id.show();
+    $('section').hide();
+    $(id).show();
 }
 
 $(document).ready(function() {
-    /*var what = $("#What");
-    var where = $("#Where");
-    var who = $("#Who");
+
     showSection(what);
 
-    $('#What-link').click( showSection($('#What')));
-    $('#Who-link').click( showSection($('who')));
-    $('#Where-link').click( showSection('where'));*/
+    $('#What-link').click( showSection(what));
+    $('#Who-link').click( showSection(who));
+    $('#Where-link').click( showSection(where));
 
-    $("#What").hide();
-    $("#Where").hide();
-    $("#Who").hide();
-    $("#What").show();
-
-    $('#What-link').click( function() {
-        $("#What").hide();
-        $("#Where").hide();
-        $("#Who").hide();
-        $("#What").show();
-    });
-
-
-    $('#Where-link').click(function() {
-        $("#What").hide();
-        $("#Where").hide();
-        $("#Who").hide();
-        $("#Where").show();
-    });
-
-    $('#Who-link').click(function() {
-        $("#What").hide();
-        $("#Where").hide();
-        //$("#Who").hide();
-        $("#Who").show();
-    });
 });
